2d175387fdc45cc0e51955795394fcada25c14d6
Fabian LLanos <fa.llanos@udla.edu.co>
2020-07-17 12:35:39 -0500

    bloc user

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:geolearn/User/bloc/bloc_user.dart';
 import 'package:geolearn/widgets/description_content.dart';
-import 'package:geolearn/widgets/thematics.dart';
-import 'package:geolearn/widgets/geolearn.dart';
-import 'package:geolearn/widgets/login.dart';
+import 'package:geolearn/Thematic/ui/screens/thematics.dart';
+import 'package:geolearn/geolearn.dart';
+import 'package:geolearn/User/ui/screens/login.dart';
 
 void main() => runApp(MyApp());
 
--- lib/widgets/gradient.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class GradientBack extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return null;
-  }
-}
